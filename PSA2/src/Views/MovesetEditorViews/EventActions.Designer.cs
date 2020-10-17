57931c6662f694084378293d82fdd38d21fbe6bc
a-r-t <alexthimineur@gmail.com>
2020-10-17 18:06:57 -0400

    fixed event buttons

--- PSA2/src/Views/MovesetEditorViews/EventActions.Designer.cs
+++ PSA2/src/Views/MovesetEditorViews/EventActions.Designer.cs
@@ -32,13 +32,16 @@
             this.addCommandBelowBtn = new System.Windows.Forms.Button();
             this.replaceCommandBtn = new System.Windows.Forms.Button();
             this.removeCommandBtn = new System.Windows.Forms.Button();
-            this.moveCommandUpBtn = new System.Windows.Forms.Button();
-            this.moveCommandDownBtn = new System.Windows.Forms.Button();
             this.copyBtn = new System.Windows.Forms.Button();
             this.pasteBtn = new System.Windows.Forms.Button();
             this.cutBtn = new System.Windows.Forms.Button();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.addCommandtoEndBtn = new System.Windows.Forms.Button();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.moveCommandUpBtn = new System.Windows.Forms.Button();
+            this.moveCommandDownBtn = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // addCommandAboveBtn
@@ -46,11 +49,11 @@
             this.addCommandAboveBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.addCommandAboveBtn.Location = new System.Drawing.Point(3, 3);
+            this.addCommandAboveBtn.Location = new System.Drawing.Point(3, 91);
             this.addCommandAboveBtn.Name = "addCommandAboveBtn";
             this.addCommandAboveBtn.Size = new System.Drawing.Size(329, 82);
             this.addCommandAboveBtn.TabIndex = 0;
-            this.addCommandAboveBtn.Text = "Add Command Above";
+            this.addCommandAboveBtn.Text = "Insert Above";
             this.addCommandAboveBtn.UseVisualStyleBackColor = true;
             this.addCommandAboveBtn.Click += new System.EventHandler(this.addCommandAboveBtn_Click);
             // 
@@ -59,11 +62,11 @@
             this.addCommandBelowBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.addCommandBelowBtn.Location = new System.Drawing.Point(3, 91);
+            this.addCommandBelowBtn.Location = new System.Drawing.Point(3, 179);
             this.addCommandBelowBtn.Name = "addCommandBelowBtn";
-            this.addCommandBelowBtn.Size = new System.Drawing.Size(329, 82);
+            this.addCommandBelowBtn.Size = new System.Drawing.Size(329, 83);
             this.addCommandBelowBtn.TabIndex = 1;
-            this.addCommandBelowBtn.Text = "Add Command Below";
+            this.addCommandBelowBtn.Text = "Insert Below";
             this.addCommandBelowBtn.UseVisualStyleBackColor = true;
             this.addCommandBelowBtn.Click += new System.EventHandler(this.addCommandBelowBtn_Click);
             // 
@@ -72,11 +75,11 @@
             this.replaceCommandBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.replaceCommandBtn.Location = new System.Drawing.Point(3, 179);
+            this.replaceCommandBtn.Location = new System.Drawing.Point(338, 91);
             this.replaceCommandBtn.Name = "replaceCommandBtn";
-            this.replaceCommandBtn.Size = new System.Drawing.Size(329, 83);
+            this.replaceCommandBtn.Size = new System.Drawing.Size(329, 82);
             this.replaceCommandBtn.TabIndex = 2;
-            this.replaceCommandBtn.Text = "Replace Command";
+            this.replaceCommandBtn.Text = "Replace";
             this.replaceCommandBtn.UseVisualStyleBackColor = true;
             this.replaceCommandBtn.Click += new System.EventHandler(this.replaceCommandBtn_Click);
             // 
@@ -89,35 +92,9 @@
             this.removeCommandBtn.Name = "removeCommandBtn";
             this.removeCommandBtn.Size = new System.Drawing.Size(329, 83);
             this.removeCommandBtn.TabIndex = 3;
-            this.removeCommandBtn.Text = "Remove Command";
+            this.removeCommandBtn.Text = "Remove";
             this.removeCommandBtn.UseVisualStyleBackColor = true;
             this.removeCommandBtn.Click += new System.EventHandler(this.removeCommandBtn_Click);
-            // 
-            // moveCommandUpBtn
-            // 
-            this.moveCommandUpBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.moveCommandUpBtn.Location = new System.Drawing.Point(338, 3);
-            this.moveCommandUpBtn.Name = "moveCommandUpBtn";
-            this.moveCommandUpBtn.Size = new System.Drawing.Size(329, 82);
-            this.moveCommandUpBtn.TabIndex = 4;
-            this.moveCommandUpBtn.Text = "Move Command Up";
-            this.moveCommandUpBtn.UseVisualStyleBackColor = true;
-            this.moveCommandUpBtn.Click += new System.EventHandler(this.moveCommandUpBtn_Click);
-            // 
-            // moveCommandDownBtn
-            // 
-            this.moveCommandDownBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.moveCommandDownBtn.Location = new System.Drawing.Point(338, 91);
-            this.moveCommandDownBtn.Name = "moveCommandDownBtn";
-            this.moveCommandDownBtn.Size = new System.Drawing.Size(329, 82);
-            this.moveCommandDownBtn.TabIndex = 5;
-            this.moveCommandDownBtn.Text = "Move Command Down";
-            this.moveCommandDownBtn.UseVisualStyleBackColor = true;
-            this.moveCommandDownBtn.Click += new System.EventHandler(this.moveCommandDownBtn_Click);
             // 
             // copyBtn
             // 
@@ -164,15 +141,15 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
-            this.tableLayoutPanel1.Controls.Add(this.addCommandAboveBtn, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.cutBtn, 2, 2);
-            this.tableLayoutPanel1.Controls.Add(this.addCommandBelowBtn, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.pasteBtn, 2, 1);
-            this.tableLayoutPanel1.Controls.Add(this.replaceCommandBtn, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.copyBtn, 2, 0);
-            this.tableLayoutPanel1.Controls.Add(this.moveCommandUpBtn, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.moveCommandDownBtn, 1, 1);
             this.tableLayoutPanel1.Controls.Add(this.removeCommandBtn, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.replaceCommandBtn, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.addCommandBelowBtn, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.addCommandAboveBtn, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.addCommandtoEndBtn, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -183,6 +160,54 @@
             this.tableLayoutPanel1.Size = new System.Drawing.Size(1006, 265);
             this.tableLayoutPanel1.TabIndex = 9;
             // 
+            // addCommandtoEndBtn
+            // 
+            this.addCommandtoEndBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.addCommandtoEndBtn.Location = new System.Drawing.Point(3, 3);
+            this.addCommandtoEndBtn.Name = "addCommandtoEndBtn";
+            this.addCommandtoEndBtn.Size = new System.Drawing.Size(329, 82);
+            this.addCommandtoEndBtn.TabIndex = 10;
+            this.addCommandtoEndBtn.Text = "Append";
+            this.addCommandtoEndBtn.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.moveCommandDownBtn, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.moveCommandUpBtn, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(338, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(329, 82);
+            this.tableLayoutPanel2.TabIndex = 11;
+            // 
+            // moveCommandUpBtn
+            // 
+            this.moveCommandUpBtn.Dock = System.Windows.Forms.DockStyle.Left;
+            this.moveCommandUpBtn.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.moveCommandUpBtn.Location = new System.Drawing.Point(0, 0);
+            this.moveCommandUpBtn.Margin = new System.Windows.Forms.Padding(0);
+            this.moveCommandUpBtn.Name = "moveCommandUpBtn";
+            this.moveCommandUpBtn.Size = new System.Drawing.Size(162, 82);
+            this.moveCommandUpBtn.TabIndex = 6;
+            this.moveCommandUpBtn.Text = "▲ Move Up";
+            this.moveCommandUpBtn.UseVisualStyleBackColor = true;
+            // 
+            // moveCommandDownBtn
+            // 
+            this.moveCommandDownBtn.Dock = System.Windows.Forms.DockStyle.Right;
+            this.moveCommandDownBtn.Location = new System.Drawing.Point(167, 0);
+            this.moveCommandDownBtn.Margin = new System.Windows.Forms.Padding(0);
+            this.moveCommandDownBtn.Name = "moveCommandDownBtn";
+            this.moveCommandDownBtn.Size = new System.Drawing.Size(162, 82);
+            this.moveCommandDownBtn.TabIndex = 7;
+            this.moveCommandDownBtn.Text = "▼ Move Down";
+            this.moveCommandDownBtn.UseVisualStyleBackColor = true;
+            // 
             // EventActions
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -191,6 +216,7 @@
             this.Name = "EventActions";
             this.Size = new System.Drawing.Size(1006, 265);
             this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -201,11 +227,13 @@
         private System.Windows.Forms.Button addCommandBelowBtn;
         private System.Windows.Forms.Button replaceCommandBtn;
         private System.Windows.Forms.Button removeCommandBtn;
-        private System.Windows.Forms.Button moveCommandUpBtn;
-        private System.Windows.Forms.Button moveCommandDownBtn;
         private System.Windows.Forms.Button copyBtn;
         private System.Windows.Forms.Button pasteBtn;
         private System.Windows.Forms.Button cutBtn;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button addCommandtoEndBtn;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button moveCommandDownBtn;
+        private System.Windows.Forms.Button moveCommandUpBtn;
     }
 }
