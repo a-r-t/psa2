879505301b6a74cc0e0b750d7ec6d5b4f5caec70
a-r-t <alexthimineur@gmail.com>
2020-11-13 16:36:46 -0500

    creating tab control custom designs

--- PSA2/src/Views/CustomControls/TabControlCustom.cs
+++ PSA2/src/Views/CustomControls/TabControlCustom.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Collections.Specialized;
+using PSA2.src.ExtentionMethods;
 
 namespace PSA2.src.Views.CustomControls
 {
@@ -16,6 +17,22 @@ namespace PSA2.src.Views.CustomControls
         private Panel tabsHolder;
         private Panel tabViewer;
         private List<TabCustom> tabs;
+        private int currentTabIndex = -1;
+        public int CurrentTabIndex
+        {
+            get
+            {
+                return currentTabIndex;
+            }
+            set
+            {
+                if (value != currentTabIndex && value < TabPages.Count)
+                {
+                    SelectTab(value);
+                    currentTabIndex = value;
+                }
+            }
+        }
 
         public TabControlCustom(): base()
         {
@@ -23,33 +40,66 @@ namespace PSA2.src.Views.CustomControls
             TabPages.CollectionChanged += new NotifyCollectionChangedEventHandler(OnTabPageChange);
             tabs = new List<TabCustom>();
             SetUpControls();
+            tabViewer.DoubleBuffered(true);
         }
 
         private void OnTabPageChange(object sender, NotifyCollectionChangedEventArgs e)
         {
             if (e.Action == NotifyCollectionChangedAction.Add)
             {
-                TabPageCustom newItem = (TabPageCustom)e.NewItems[0];
-                TabCustom tab = new TabCustom(newItem.TabText);
-                int xLocation = 0;
-                if (tabs.Count > 0)
+                if (e.NewStartingIndex == TabPages.Count - 1)
                 {
-                    TabCustom lastItem = tabs[tabs.Count - 1];
-                    xLocation = lastItem.Location.X + lastItem.Width + 1;
+                    TabPageCustom newTabPage = (TabPageCustom)e.NewItems[0];
+                    AppendTab(newTabPage, e.NewStartingIndex);
                 }
-                tab.Location = new Point(xLocation, 0);
-                tab.BackColor = Color.Orange;
-                tabs.Add(tab);
-                int index = tabs.Count - 1;
-                tab.Click += (s, EventArgs) => { OnTabClicked(sender, EventArgs, index); };
-                tabsHolder.Controls.Add(tab);
             }
+        }
+
+        private void AppendTab(TabPageCustom tabPage, int tabIndex)
+        {
+            TabCustom tab = CreateTab(tabPage, tabIndex);
+            tabs.Add(tab);
+            tabsHolder.Controls.Add(tab);
+            if (tabs.Count == 1)
+            {
+                CurrentTabIndex = 0;
+            }
+        }
+
+        private TabCustom CreateTab(TabPageCustom tabPage, int tabIndex)
+        {
+            TabCustom tab = new TabCustom(tabPage.TabText);
+            int xLocation = 0;
+            if (tabs.Count > 0)
+            {
+                TabCustom lastItem = tabs[tabs.Count - 1];
+                xLocation = lastItem.Location.X + lastItem.Width + 1;
+            }
+            tab.Location = new Point(xLocation, 0);
+            tab.Height = tabsHolder.Height;
+            tab.Click += (sender, EventArgs) => { OnTabClicked(sender, EventArgs, tabIndex); };
+            return tab;
         }
 
         private void OnTabClicked(object sender, EventArgs e, int tabIndex)
         {
+            if (CurrentTabIndex != tabIndex)
+            {
+                CurrentTabIndex = tabIndex;
+            }
+        }
+
+        private void SelectTab(int tabIndex)
+        {
+            tabViewer.SuspendLayout();
             tabViewer.Controls.Clear();
             tabViewer.Controls.Add(TabPages[tabIndex]);
+            tabViewer.ResumeLayout();
+
+            for (int i = 0; i < tabs.Count; i++)
+            {
+                tabs[i].IsSelected = i == tabIndex;
+            }
         }
 
         public void SetUpControls()
@@ -84,7 +134,7 @@ namespace PSA2.src.Views.CustomControls
 
         protected override void OnPaint(PaintEventArgs e)
         {
-            e.Graphics.DrawString("HELLO", new Font(new FontFamily("Times New Roman"), 12, FontStyle.Regular), new SolidBrush(Color.DarkBlue), new Point(0, 0));
+            //e.Graphics.DrawString("HELLO", new Font(new FontFamily("Times New Roman"), 12, FontStyle.Regular), new SolidBrush(Color.DarkBlue), new Point(0, 0));
             base.OnPaint(e);
         }
     }
--- PSA2/src/Views/CustomControls/TabCustom.cs
+++ PSA2/src/Views/CustomControls/TabCustom.cs
@@ -9,17 +9,168 @@ using System.Windows.Forms;
 
 namespace PSA2.src.Views.CustomControls
 {
-    public partial class TabCustom: Button
+    public partial class TabCustom: Panel
     {
+        private Color textColor;
+        public Color TextColor 
+        {
+            get
+            {
+                return textColor;
+            }
+            set
+            {
+                textColor = value;
+                if (!isSelected)
+                {
+                    currentTextColor = value;
+                }
+            }
+        }
+
+        private Color selectedTextColor;
+        public Color SelectedTextColor 
+        {
+            get
+            {
+                return selectedTextColor;
+            }
+            set
+            {
+                selectedTextColor = value;
+                if (isSelected)
+                {
+                    currentTextColor = value;
+                }
+            }
+        }
+
+        private Color backgroundColor;
+        public Color BackgroundColor 
+        { 
+            get
+            {
+                return backgroundColor;
+            }
+            set
+            {
+                backgroundColor = value;
+                if (!isSelected)
+                {
+                    BackColor = value;
+                }
+            } 
+        }
+
+        private Color selectedBackColor;
+        public Color SelectedBackgroundColor 
+        { 
+            get
+            {
+                return selectedBackColor;
+            }
+            set
+            {
+                selectedBackColor = value;
+                if (isSelected)
+                {
+                    BackColor = value;
+                }
+            }
+        }
+
+        private Color currentTextColor;
+
+        public bool isSelected;
+        public bool IsSelected 
+        { 
+            get
+            {
+                return isSelected;
+            }
+            set
+            {
+                isSelected = value;
+                if (isSelected)
+                {
+                    StyleSelected();
+                }
+                else
+                {
+                    StyleUnselected();
+                }
+            } 
+        }
+
+        public Color HoverBackgroundColor { get; set; }
+
+        /*
+        private Color hoverTextColor;
+        public Color HoverTextColor
+        {
+            get
+            {
+                return hoverTextColor;
+            }
+            set
+            {
+                hoverTextColor = value;
+            }
+        }
+        */
+        public Color HoverTextColor { get; set; }
+
         public TabCustom(string text) : base()
         {
-            FlatStyle = FlatStyle.Flat;
             Font = new Font("Times New Roman", 10, FontStyle.Regular);
             Text = text;
             Width = text.Measure(Font).Width + 20;
             Height = 30;
+            TextColor = Color.Black;
+            BackgroundColor = Color.White;
+            SelectedTextColor = Color.White;
+            SelectedBackgroundColor = Color.Black;
+            HoverBackgroundColor = Color.Gray;
+            HoverTextColor = TextColor;
         }
 
-   
+        public virtual void StyleUnselected()
+        {
+            BackColor = BackgroundColor;
+            currentTextColor = TextColor;
+        }
+
+        public virtual void StyleSelected()
+        {
+            BackColor = SelectedBackgroundColor;
+            currentTextColor = SelectedTextColor;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Size textSize = Text.Measure(Font);
+            e.Graphics.DrawString(Text, Font, new SolidBrush(currentTextColor), new Point(((Width - textSize.Width) / 2) + 3, (Height - textSize.Height) / 2));
+            base.OnPaint(e);
+        }
+
+        protected override void OnMouseEnter(EventArgs e)
+        {
+            if (!IsSelected)
+            {
+                currentTextColor = HoverTextColor;
+                BackColor = HoverBackgroundColor;
+            }
+            base.OnMouseEnter(e);
+        }
+
+        protected override void OnMouseLeave(EventArgs e)
+        {
+            if (!IsSelected)
+            {
+                currentTextColor = TextColor;
+                BackColor = BackgroundColor;
+            }
+            base.OnMouseLeave(e);
+        }
     }
 }
